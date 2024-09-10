152d40bad6fc5a48c9824c43a799dde00cead2a8
nenad0707 <risticnenad.vr@gmail.com>
2024-09-10 22:36:53 +0200

    Add AddMovieCommand and handler, update project references

---
+++ MovieCraft.Application/Features/Movies/Commands/AddMovieCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace MovieCraft.Application.Features.Movies.Commands;
+
+public class AddMovieCommand : IRequest
+{
+    public int TmdbId { get; set; }
+    public string Title { get; set; } = default!;
+    public string Overview { get; set; } = default!;
+    public DateTime? ReleaseDate { get; set; }
+    public string PosterPath { get; set; } = default!;
+}
---
+++ MovieCraft.Application/Features/Movies/Commands/AddMovieCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using MovieCraft.Domain.Entities;
+
+namespace MovieCraft.Application.Features.Movies.Commands;
+
+public class AddMovieCommandHandler : IRequestHandler<AddMovieCommand>
+{
+    private readonly IMovieRepository _movieRepository;
+    
+    private readonly IMapper _mapper;
+
+    public AddMovieCommandHandler(IMovieRepository movieRepository, IMapper mapper)
+    {
+        _movieRepository = movieRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<Unit> Handle(AddMovieCommand request, CancellationToken cancellationToken)
+    {
+        var movie = _mapper.Map<Movie>(request);
+      
+        await _movieRepository.AddAsync(movie);
+        
+        return Unit.Value;
+    }
+}

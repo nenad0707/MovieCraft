a7605a2633a6205b91bd65a476a14d1efe455980
nenad0707 <risticnenad.vr@gmail.com>
2024-09-10 22:58:48 +0200

    Add properties to FavoriteMovieDto for detailed movie info

--- MovieCraft.Application/DTOs/FavoriteMovieDto.cs
+++ MovieCraft.Application/DTOs/FavoriteMovieDto.cs
@@ -2,4 +2,9 @@
 
 public class FavoriteMovieDto
 {
+    public int MovieId { get; set; }
+    public string Title { get; set; } = default!;
+    public string Overview { get; set; } = default!;
+    public DateTime? ReleaseDate { get; set; }
+    public string PosterPath { get; set; } = default!;
 }

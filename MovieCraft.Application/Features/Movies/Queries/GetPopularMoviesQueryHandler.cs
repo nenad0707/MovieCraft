851dbcb1af678f463ac92d206f0faaeec0accb7b
nenad0707 <risticnenad.vr@gmail.com>
2024-09-11 23:07:53 +0200

    Add IMovieRepository to GetPopularMoviesQueryHandler

--- MovieCraft.Application/Features/Movies/Queries/GetPopularMoviesQueryHandler.cs
+++ MovieCraft.Application/Features/Movies/Queries/GetPopularMoviesQueryHandler.cs
@@ -2,23 +2,39 @@
 using MediatR;
 using MovieCraft.Application.DTOs;
 using MovieCraft.Application.Interfaces;
+using MovieCraft.Domain.Entities;
 
 namespace MovieCraft.Application.Features.Movies.Queries;
 
 public class GetPopularMoviesQueryHandler : IRequestHandler<GetPopularMoviesQuery, IEnumerable<MovieDto>>
 {
     private readonly ITmdbService _tmdbService;
+    private readonly IMovieRepository _movieRepository;
     private readonly IMapper _mapper;
 
-    public GetPopularMoviesQueryHandler(ITmdbService tmdbService, IMapper mapper)
+    public GetPopularMoviesQueryHandler(ITmdbService tmdbService, IMapper mapper, IMovieRepository movieRepository)
     {
         _tmdbService = tmdbService;
         _mapper = mapper;
+        _movieRepository = movieRepository;
     }
 
     public async Task<IEnumerable<MovieDto>> Handle(GetPopularMoviesQuery request, CancellationToken cancellationToken)
     {
-        var movies = await _tmdbService.GetPopularMoviesAsync();
-        return _mapper.Map<IEnumerable<MovieDto>>(movies);
+        var popularMovies = await _tmdbService.GetPopularMoviesAsync();
+
+
+        foreach (var movieDto in popularMovies)
+        {
+            var existingMovie = await _movieRepository.GetByTmdbIdAsync(movieDto.Id);
+
+            if (existingMovie == null) 
+            {
+                var movieEntity = _mapper.Map<Movie>(movieDto);
+                await _movieRepository.AddAsync(movieEntity);
+            }
+        }
+
+        return popularMovies;
     }
 }

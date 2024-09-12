c7c9cd688a4568a0d8fff42404748056d9b4f6a6
nenad0707 <risticnenad.vr@gmail.com>
2024-09-12 23:41:32 +0200

    Add Name property to User entity and update migrations

---
+++ MovieCraft.Infrastructure/Migrations/20240912214030_AddMovieNameProperty.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieCraft.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovieNameProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+        }
+    }
+}

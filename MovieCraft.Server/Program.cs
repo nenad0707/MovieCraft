58de3ac6334934bbca45c1ada50c50d20994d4dc
nenad0707 <risticnenad.vr@gmail.com>
2024-09-14 00:29:16 +0200

    Add FavoriteMovieRepository to DI container

--- MovieCraft.Server/Program.cs
+++ MovieCraft.Server/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddScoped<ITmdbService, TmdbService>();
 // Register repositories
 builder.Services.AddScoped<IMovieRepository, MovieRepository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-
+builder.Services.AddScoped<IFavoriteMovieRepository, FavoriteMovieRepository>();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

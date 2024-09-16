c054a4105973d8a4a702be3d72fe1aed5cd50f25
nenad0707 <risticnenad.vr@gmail.com>
2024-09-16 22:35:15 +0200

    Add data annotations for validation in DTO classes

--- MovieCraft.Application/DTOs/UserDto.cs
+++ MovieCraft.Application/DTOs/UserDto.cs
@@ -1,9 +1,18 @@
-﻿namespace MovieCraft.Application.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieCraft.Application.DTOs;
 
 public class UserDto
 {
-    public int Id { get; set; } 
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "UserId is required.")]
+    [MaxLength(450, ErrorMessage = "UserId cannot exceed 450 characters.")]
     public string UserId { get; set; } = default!;
+
+    [Required(ErrorMessage = "Name is required.")]
+    [MaxLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
     public string Name { get; set; } = default!;
+
     public ICollection<FavoriteMovieDto> FavoriteMovies { get; set; } = new List<FavoriteMovieDto>();
 }

74aa854af7d1d6426653605bf75b9879af31141d
nenad0707 <risticnenad.vr@gmail.com>
2024-09-18 22:21:33 +0200

    Refactor OnModelCreating for modular entity configuration

--- MovieCraft.Infrastructure/Persistence/MovieDbContext.cs
+++ MovieCraft.Infrastructure/Persistence/MovieDbContext.cs
@@ -17,54 +17,6 @@ public class MovieDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<Movie>(entity =>
-        {
-            entity.ToTable("Movies");
-
-            entity.Property(m => m.TmdbId)
-                .IsRequired();
-
-            entity.Property(m => m.Title)
-                .IsRequired()
-                .HasMaxLength(200); 
-
-            entity.Property(m => m.Overview)
-                .HasMaxLength(1000); 
-
-            entity.Property(m => m.PosterPath)
-                .HasMaxLength(500);
-        });
-
-        modelBuilder.Entity<User>(entity =>
-        {
-            entity.ToTable("Users");
-
-            entity.HasIndex(u => u.UserId)
-                .IsUnique();
-
-            entity.Property(u => u.UserId)
-                .IsRequired()
-                .HasMaxLength(50); 
-
-            entity.Property(u => u.Name)
-                .IsRequired()
-                .HasMaxLength(100); 
-        });
-
-        modelBuilder.Entity<FavoriteMovie>(entity =>
-        {
-            entity.ToTable("FavoriteMovies");
-
-            entity.HasOne(fm => fm.User)
-                .WithMany(u => u.FavoriteMovies)
-                .HasForeignKey(fm => fm.UserId)
-                .HasPrincipalKey(u => u.UserId)
-                .OnDelete(DeleteBehavior.Cascade); 
-
-            entity.HasOne(fm => fm.Movie)
-                .WithMany(m => m.FavoriteMovies)
-                .HasForeignKey(fm => fm.MovieId)
-                .OnDelete(DeleteBehavior.Cascade); 
-        });
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MovieDbContext).Assembly);
     }
 }

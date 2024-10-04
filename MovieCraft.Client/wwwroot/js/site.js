2138b7b19733030d7c24ac1318897b6a8dd33b22
nenad0707 <risticnenad.vr@gmail.com>
2024-10-05 01:46:04 +0200

    Add carousel for popular movies and preload images
    Replaced static welcome message in Home.razor with a new Carousel component. Updated SplashScreen.razor to preload images for popular movies and added a preload link for movie posters. Included a new namespace in _Imports.razor. Reformatted index.html and added Swiper.js stylesheet and script. Created Carousel.razor component with dependency injection and IDisposable implementation. Added Carousel.razor.css for styling and responsive design. Created site.js to initialize Swiper.js carousel.

---
+++ MovieCraft.Client/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿window.initSwiper = function () {
+    console.log('initSwiper function called');
+    var swiper = new Swiper('.swiper-container', {
+        effect: 'coverflow',
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        initialSlide: 3,
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 150,
+            modifier: 1.5,
+            slideShadows: true,
+        },
+        loop: true,
+        simulateTouch: true,
+        navigation: false
+    });
+};

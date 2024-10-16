05303f55469d3e40116c925ad896b44d033b8daf
nenad0707 <risticnenad.vr@gmail.com>
2024-10-17 01:32:15 +0200

    Enhance background image handling with transitions. Introduced a new mechanism for handling background images with smooth transitions

--- MovieCraft.Client/wwwroot/js/site.js
+++ MovieCraft.Client/wwwroot/js/site.js
@@ -33,5 +33,6 @@ window.preloadImage = (url) => {
         const img = new Image();
         img.src = url;
         img.onload = () => resolve(true);
+        img.onerror = () => resolve(false);
     });
 };
